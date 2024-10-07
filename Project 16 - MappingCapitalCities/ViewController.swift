0ea2e0aa01f2902d707bec6b378d61e875556450
Noah Pope <npope@tutanota.com>
2024-10-07 01:51:01 -0500

    setting up Capital object

--- Project 16 - MappingCapitalCities/ViewController.swift
+++ Project 16 - MappingCapitalCities/ViewController.swift
@@ -5,13 +5,20 @@
 //  Created by Noah Pope on 10/7/24.
 //
 
+import MapKit
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpCities()
+    }
+    
+    func setUpCities() {
+        let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: "Home to the 2012 Oslo Olympics")
     }
 
 
